b956877e97a6e6e472416d2360896ac9dd1e9f0f
Rajesh Baramol <rajeshbaramol@gmail.com>
2022-10-18 07:51:21 +0000

    Update 3 files

---
+++ src/calender-meetings.js
@@ -0,0 +1,8 @@
+// calculating time availablity for the meetings
+
+const times=[
+    {scheduled:['09:00','10:30'],['12:00','13:00'],['16:00','18:00'],
+timeAvailable:['09:00','20:00']
+},{scheduled:['09:00','10:30'],['12:00','13:00'],['16:00','18:00'],
+timeAvailable:['09:00','20:00']
+}]
--- src/index.js
+++ src/index.js
@@ -3,28 +3,28 @@ const blocks = [
   {
     gym: false,
     school: true,
-    store: false
+    store: false,
   },
   {
     gym: true,
     school: false,
-    store: false
+    store: false,
   },
   {
     gym: true,
     school: true,
-    store: false
+    store: false,
   },
   {
     gym: false,
     school: true,
-    store: false
+    store: false,
   },
   {
     gym: false,
     school: true,
-    store: true
-  }
+    store: true,
+  },
 ];
 let max = 0;
 for (let index = 0; index < blocks.length; index++) {
@@ -41,7 +41,7 @@ for (let index = 0; index < blocks.length; index++) {
 function getSet(obj) {
   return Object.keys(obj).filter((x) => obj[x] === true).length;
 }
-// console.log(max);
+console.log(max);
 
 //question 2
 let builds = [
@@ -50,17 +50,16 @@ let builds = [
   [true, true, true, true, true, true, false, false, false],
   [true, false, false, false, false],
   [true, false],
-  [true, true, true, true, false, false]
+  [true, true, true, true, false, false],
 ];
 let buildpercent = [];
 for (var i = 0; i < builds.length; i++) {
   for (var j = 0; j < builds[i].length; j++) {
     if (builds[i][j] === false || builds[i][builds[i].length - j]) {
       let pent =
-        (builds[i][j].length -
-          (builds[i][j] === false ? builds[i].length - j : j)) /
+        (builds[i].length -
+          (builds[i][j] === false ? builds[i].length - j : j - 1)) /
         builds[i].length;
-      console.log(builds[i].length - j + 1, j + 1);
 
       buildpercent.push(Math.round(pent * 100));
       break;
